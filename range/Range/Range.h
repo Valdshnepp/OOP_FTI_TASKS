2e276f2e1afe71e4d10baafbf22ffcd89e3a0d3a
Mixail-laboratory <93642647+Mixail-laboratory@users.noreply.github.com>
2023-09-23 11:27:24 +0300

    Прокомменитировал

--- range/Range/Range.h
+++ range/Range/Range.h
@@ -6,6 +6,16 @@
 #include <cmath>
 #include <type_traits>
 
+//В целом молодец, идею уловил верно, но надо доработать.
+//1. typename = std::is_arithmetic<T>. Не понял, что ты тут хотел сделать. То что здесь написано просто делает вторым параметром по умолчанию true или false, в зависимости от того, что std::is_arithmetic выплюнет. На SFINAE не оч похоже.
+//2. Посмотри реализацию Range(T begin, T stop). Не знаю зачем ты сделал нулевой step, видимо чтоб никогда из цикла не выходить).
+//3. Соответвсвенно в Range(T begin, T stop, T step) надо чтобы  step был не равен 0.
+//4. Сделай так, чтобы дефолтный конструктор был недоступен.
+//5. Хочу константые перегрузки begin и end.
+
+
+
+
 template <typename T,typename = std::is_arithmetic<T>>
 class Range {
 	T m_begin,m_end,m_step;
